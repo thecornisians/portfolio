58cbc9cac936aca7f9745ee990c3ddff111076a9
Cornelius Owusu-Ansah <84820874+thecornisians@users.noreply.github.com>
2022-10-17 17:19:54 +0000

    link error

--- components/Navbar.js
+++ components/Navbar.js
@@ -24,9 +24,9 @@ export default function Navbar(){
             <header className="bg-[#161C2D] border-5 border-white">
                 <nav className="px-2 sm:px-4 py-2.5 rounded">
                     <div className="container flex flex-wrap justify-between items-center mx-auto">
-                        <a href="/" className="flex items-center">
+                        <Link href="/" className="flex items-center">
                             <span className="self-center text-xl font-semibold text-[#1DB67D]">Cornelius OA</span>
-                        </a>
+                        </Link>
                         <button data-collapse-toggle="navbar-default" type="button" id="display-navigation" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
