08e3dcd607062fbd622eed249a321e456185eb53
thecornisians <thecornisians@gmail.com>
2022-12-07 10:51:59 +0000

    made changes to index page and blog component

--- pages/contact.js
+++ pages/contact.js
@@ -1,18 +1,16 @@
-import Navbar from "../components/Navbar"
-import Form from "../components/Form"
-import Link from 'next/link'
-import Footer from "../components/Footer"
+import Navbar from "../components/Navbar";
+import Form from "../components/Form";
+import Link from "next/link";
+import Footer from "../components/Footer";
 
-export default function Contact(){
-    return(
-        <main className=' dark:bg-gray-900 scroll-smooth' >
-                <section className='min-h-screen'>
-                    <Navbar />
-                    <Form />         
-                
-                </section>
-                <Footer />
-        </main>
-      
-    )
+export default function Contact() {
+  return (
+    <main className=" dark:bg-gray-900 scroll-smooth">
+      <section className="min-h-screen">
+        <Navbar />
+        <Form />
+      </section>
+      <Footer />
+    </main>
+  );
 }
