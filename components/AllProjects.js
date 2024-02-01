4232658240a89cb3030b665b57f0931d26cf0b26
thecornisians <thecornisians@gmail.com>
2024-02-01 23:39:36 +0000

    updates

--- components/AllProjects.js
+++ components/AllProjects.js
@@ -1,326 +1,133 @@
 import Link from "next/link";
 import Image from "next/image";
-import Happynotes from "../public/happynotes.gif";
-import creativeConnect from "../public/creativesConnect.gif";
-import chillgh from "../public/chillresort.png";
-import shopamargaret from "../public/shopamargaret.png";
-import Nanaasomani from "../public/nanaasomani.png";
-import cornelius from "../public/wbgroup.png";
-import creativeconnect from "../public/creativeconnect.png";
-import larryjay from "../public/larryjay.png";
-import haleoptions from "../public/haleoptions.png";
-import alwoman from "../public/alwoman.png";
-import nurtureholiks from "../public/nurtureholiks.png";
 
 export default function AllProjects() {
   return (
     <section id="portfolio" className="scroll-smooth p-5 ">
       <div className="text-center">
         <h3 className="font-semibold text-lg text-primary mb-2"> Portfolio</h3>
-        <h4 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4">
+        <h4 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4 source-sans">
           Recent Projects
         </h4>
-        <p className="text-base text-body-color">
+        <p className="text-base source-sans mb-4 md:mb-8">
           Here are some of my most recent projects and freelance work
         </p>
       </div>
 
-      <div>
-        <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-          <div className="basis-1/3 flex-1 mb-3">
-            <Image
-              src={creativeconnect}
-              className="rounded-lg max-w-full h-8 w-8 shadow-lg "
-              alt="creative-connect"
-            />
-            <p className="text-sm mt-1 mb-1 md:text-md">
-              Full stack application with authentication built with Node,
-              express, MongoDB and EJS.
-            </p>
-            <div className="flex gap-3 mt-2">
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://github.com/thecornisians/Creatives-connect"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github Repo
-              </a>
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://creativeconnect.cyclic.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Site
-              </a>
-            </div>
-          </div>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto place-items-center mb-6">
+          {Projects.map(({ image, title, githubLink, liveLink }, index) => {
+            return (
+              <div key={index} className="flex flex-col items-center">
+                <Image
+                  src={image}
+                  width={400}
+                  height={250}
+                  className="rounded-lg image shadow-lg"
+                  alt="creative-connect"
+                />
+                <p className="text-base mt-1  source-sans px-3">{title}</p>
 
-          <div className="basis-1/3 flex-1 mb-3">
-            <Image
-              src={Nanaasomani}
-              className="rounded-lg max-w-full h-8 w-8 shadow-lg"
-              alt="nanaasomani"
-            />
-            <p className="text-sm mt-1 mb-1 md:text-md">
-              Portfolio website for client built with HTML, CSS, Javascript.
-            </p>
-            <div className="flex gap-3 mt-2">
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://github.com/thecornisians/nanaasomani-website"
-              >
-                Github Repo
-              </a>
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://nanaasomani.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Site
-              </a>
-            </div>
-          </div>
-          <div className="basis-1/3 flex-1 mb-3">
-            <Image
-              src={Happynotes}
-              className="rounded-lg max-w-full h-8 w-8 shadow-lg"
-              alt="happynotes"
-            />
-            <p className="text-sm mt-1 mb-1 md:text-md">
-              Full stack to-do application built with Node, express, MongoDB and
-              bootstrap for styling.
-            </p>
-            <div className="flex gap-3 mt-2">
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://github.com/thecornisians/happy-notes"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github Repo
-              </a>
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://happynotes.cyclic.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Site
-              </a>
-            </div>
-          </div>
+                <div className="md:mb-12 flex gap-3 mt-3">
+                  <a
+                    href={githubLink}
+                    target="_blank"
+                    className="bg-black text-white source-sans py-1 px-4 rounded inline-flex "
+                  >
+                    Github
+                  </a>
 
-          <div className="basis-1/3 flex-1 mb-3">
-            <Image
-              src={cornelius}
-              className="rounded-lg max-w-full h-8 w-8 shadow-lg"
-              alt="cornelius owusu-ansah"
-            />
-            <p className="text-sm mt-1 mb-1 md:text-md">
-              client site built with React/NextJS and Tailwind CSS
-            </p>
-            <div className="flex gap-3 mt-2">
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://github.com/thecornisians/wb-group"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github Repo
-              </a>
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://wbgrouprepresents.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Site
-              </a>
-            </div>
-          </div>
-
-          <div className="basis-1/3 flex-1 mb-3">
-            <Image
-              src={chillgh}
-              className="rounded-lg max-w-full h-8 w-8 shadow-lg"
-              alt="Chill River Resort"
-            />
-            <p className="text-sm mt-1 mb-1 md:text-md">
-              Website built for the Chill River Resort.
-            </p>
-            <div className="flex gap-3 mt-2">
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="#"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github Repo
-              </a>
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://thechillgh.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Site
-              </a>
-            </div>
-          </div>
-          <div className="basis-1/3 flex-1 mb-3">
-            <Image
-              src={shopamargaret}
-              className="rounded-lg max-w-full h-8 w-8 shadow-lg"
-              alt="amargaretgh"
-            />
-            <p className="text-sm mt-1 mb-1 md:text-md">
-              Ecommerce website built for Amargaretgh.
-            </p>
-            <div className="flex gap-3 mt-2">
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="#"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github Repo
-              </a>
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://shopamargaret.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Site
-              </a>
-            </div>
-          </div>
-
-          <div className="basis-1/3 flex-1 mb-3">
-            <Image
-              src={alwoman}
-              className="rounded-lg max-w-full h-8 w-8 shadow-lg"
-              alt="shopalwoman"
-            />
-            <p className="text-sm mt-1 mb-1 md:text-md">
-              Ecommerce Website built for Alwoman.
-            </p>
-            <div className="flex gap-3 mt-2">
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="#"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github Repo
-              </a>
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://shopalwoman.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Site
-              </a>
-            </div>
-          </div>
-          <div className="basis-1/3 flex-1 mb-3">
-            <Image
-              src={nurtureholiks}
-              className="rounded-lg max-w-full h-8 w-8 shadow-lg"
-              alt="nurtureholiks"
-            />
-            <p className="text-sm mt-1 mb-1 md:text-md">
-              Ecommerce website built for Nurtureholiks.
-            </p>
-            <div className="flex gap-3 mt-2">
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="#"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github Repo
-              </a>
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://nurtureholiks.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Site
-              </a>
-            </div>
-          </div>
-
-          <div className="basis-1/3 flex-1 mb-3">
-            <Image
-              src={larryjay}
-              className="rounded-lg max-w-full h-8 w-8 shadow-lg"
-              alt="larry jay Ghana"
-            />
-            <p className="text-sm mt-1 mb-1 md:text-md">
-              Ecommerce fashion website built for Larry Jay Ghana
-            </p>
-            <div className="flex gap-3 mt-2">
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="#"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github Repo
-              </a>
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://larryjayghana.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Site
-              </a>
-            </div>
-          </div>
-
-          <div className="basis-1/3 flex-1 mb-3">
-            <Image
-              src={haleoptions}
-              className="rounded-lg max-w-full h-8 w-8 shadow-lg"
-              alt="haleoptions"
-            />
-            <p className="text-sm mt-1 mb-1 md:text-md">
-              Website built with Html, Bootstrap, Javascript
-            </p>
-            <div className="flex gap-3 mt-2">
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="#"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github Repo
-              </a>
-              <a
-                className="px-1.5 py-1.5 mr-2 text-white rounded-md bg-gray-800 hover:bg-gray-400 text-sm"
-                href="https://haleoptionz.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Site
-              </a>
-            </div>
-          </div>
+                  <a
+                    href={liveLink}
+                    target="_blank"
+                    className="bg-black text-white source-sans py-1 px-4 rounded inline-flex "
+                  >
+                    Live Site
+                  </a>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
 
       <Link href="/">
-        <p className="text-[#1DB67D] cursor-pointer text-center hover:text-black text-2xl ">
+        <p className="text-[#1DB67D] cursor-pointer text-center hover:text-black text-2xl source-sans">
           Back to Home
         </p>
       </Link>
     </section>
   );
 }
+
+const Projects = [
+  {
+    image: "/creativesConnect.gif",
+    title:
+      "Full stack application with authentication built with Node,express, MongoDB and EJS.",
+    githubLink: "https://github.com/thecornisians/Creatives-connect",
+    liveLink: "https://creativeconnect.cyclic.app/",
+  },
+  {
+    image: "/nanaasomani.png",
+    title: "Portfolio website for client built with HTML, CSS, Javascript.",
+    githubLink: "https://github.com/thecornisians/nanaasomani-website",
+    liveLink: "https://nanaasomani.com/",
+  },
+  {
+    image: "/happynotes.gif",
+    title:
+      " Full stack to-do application built with Node, express, MongoDB and bootstrap for styling.",
+    githubLink: "https://github.com/thecornisians/happy-notes",
+    liveLink: "https://happynotes.cyclic.app/",
+  },
+  {
+    image: "/wbgroup.png",
+    title: "client site built with React/NextJS and Tailwind CSS",
+    githubLink: "https://github.com/thecornisians/wb-group",
+    liveLink: "https://wbgrouprepresents.com/",
+  },
+  {
+    image: "/chillresort.png",
+    title: "Website built for the Chill River Resort.",
+    githubLink: "#",
+    liveLink: "https://thechillgh.com/",
+  },
+  {
+    image: "/shopamargaret.png",
+    title: "Ecommerce website built for Amargaretgh.",
+    githubLink: "#",
+    liveLink: "https://shopamargaret.com/",
+  },
+  {
+    image: "/alwoman.png",
+    title: "Ecommerce Website built for Alwoman.",
+    githubLink: "#",
+    liveLink: "https://shopalwoman.com/",
+  },
+  {
+    image: "/nurtureholiks.png",
+    title: "Ecommerce website built for Nurtureholiks.",
+    githubLink: "#",
+    liveLink: "https://nurtureholiks.com/",
+  },
+  {
+    image: "/larryjay.png",
+    title: "Ecommerce fashion website built for Larry Jay Ghana",
+    githubLink: "#",
+    liveLink: "https://larryjayghana.com/",
+  },
+  {
+    image: "/haleoptions.png",
+    title: "Website built with Html, Bootstrap, Javascript",
+    githubLink: "https://github.com/thecornisians/Hale-Options",
+    liveLink: "https://haleoptionz.com/",
+  },
+];
+
+//  {
+//     image: "",
+//     title: "",
+//     githubLink: "",
+//     liveLink: "",
+//   },
