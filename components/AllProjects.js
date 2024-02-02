8796bd1af0c769165c97ba237094c696a88f40c2
thecornisians <thecornisians@gmail.com>
2024-02-02 08:57:50 +0000

    updates

--- components/AllProjects.js
+++ components/AllProjects.js
@@ -18,7 +18,7 @@ export default function AllProjects() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto place-items-center mb-6">
           {Projects.map(({ image, title, githubLink, liveLink }, index) => {
             return (
-              <div key={index} className="flex flex-col items-center">
+              <div key={index} className="flex flex-col items-center mb-2">
                 <Image
                   src={image}
                   width={400}
